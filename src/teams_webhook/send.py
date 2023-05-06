c8c78efde2d4801ccf8d88bd21409b03ab5c2e19
Miki Yutani <mkyutani@gmail.com>
2023-05-06 09:47:04 +0900

    Target channel selection (-n)

--- src/teams_webhook/send.py
+++ src/teams_webhook/send.py
@@ -6,20 +6,72 @@ import re
 import requests
 import sys
 
+default_target_name = '__default'
+
+def get_targets(url_string):
+    targets = {}
+    if url_string:
+        named_urls = url_string.split(';')
+        for named_url in named_urls:
+            named_url_structure = named_url.split('=')
+            if len(named_url_structure) == 2:
+                if len(named_url_structure[0]) == 0:
+                    print(f'warning: empty name: "{named_url}"', file=sys.stderr)
+                elif len(named_url_structure[1]) == 0:
+                    print(f'warning: empty url: "{named_url}"', file=sys.stderr)
+                else:
+                    targets.update({ named_url_structure[0]: named_url_structure[1]})
+            elif len(named_url_structure) == 1:
+                if len(named_url_structure[0]) == 0:
+                    print(f'warning: empty url: "{named_url}"', file=sys.stderr)
+                elif default_target_name not in targets:
+                    targets.update({ default_target_name: named_url_structure[0]})
+            else:
+                print(f'warning: Invalid url format: "{named_url}"', file=sys.stderr)
+    return targets
+
 def send():
     sys.stdin = io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8')
     sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8', line_buffering=True)
     sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8', line_buffering=True)
 
     url = os.environ.get('TEAMS_WEBHOOK')
+    targets = get_targets(url)
+    if default_target_name in targets:
+        default_url = targets[default_target_name]
+    else:
+        default_url = None
 
     import argparse
     parser = argparse.ArgumentParser(description='Send message from /dev/stdin to Microsoft Teams\' incoming webhook')
     parser.add_argument('-t', '--text', nargs=1, help=f'message instead of /dev/stdin')
-    parser.add_argument('-u', '--url', nargs=1, help=f'webhook url (default: {url})')
+    parser.add_argument('-u', '--url', nargs=1, help=f'webhook url (default: {default_url})')
+    parser.add_argument('-n', '--name', nargs=1, help=f'webhook target name')
     parser.add_argument('--dry', action='store_true', help='dry run')
 
     args = parser.parse_args()
+
+    if args.url:
+        url = args.url[0]
+        targets = get_targets(url)
+
+    if not url:
+        print('No webhook url ($TEAMS_WEBHOOK or -u parameter is necessary)', file=sys.stderr)
+        return 1
+
+    if args.name:
+        name = args.name[0]
+    else:
+        name = default_target_name
+
+    if name in targets.keys():
+        url = targets[name]
+    else:
+        if name == default_target_name:
+            print('Default webhook target name is not defined', file=sys.stderr)
+        else:
+            print(f'No such a webhook target name: {name}', file=sys.stderr)
+        return 1
 
     if args.text:
         text = args.text[0]
@@ -53,13 +105,6 @@ def send():
             }
         ]
     }
-
-    if args.url:
-        url = args.url[0]
-
-    if not url:
-        print('No webhook url ($TEAMS_WEBHOOK or -u parameter is necessary)', file=sys.stderr)
-        return 1
 
     if args.dry:
         import json
