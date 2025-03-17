1493bb72b4207c69e237f7a909790d04b33c5696
Miki Yutani <mkyutani@gmail.com>
2025-03-17 23:08:09 +0900

    Teams incoming webhook is deprecated, so send to power automate workflows

--- src/teams_webhook/send.py
+++ src/teams_webhook/send.py
@@ -14,7 +14,7 @@ def get_targets(url_string):
     if url_string:
         named_urls = url_string.split(';')
         for named_url in named_urls:
-            named_url_structure = named_url.split('=')
+            named_url_structure = named_url.split('=', 1)
             if len(named_url_structure) == 2:
                 name = named_url_structure[0].lower()
                 url_or_alias = named_url_structure[1]
@@ -26,7 +26,8 @@ def get_targets(url_string):
                     if ':' in url_or_alias:
                         targets.update({ name: url_or_alias })
                     else:
-                        targets.update({ name: f'{alias_protocol}{url_or_alias}'})
+                        alias = url_or_alias.lower()
+                        targets.update({ name: f'{alias_protocol}{alias}'})
             elif len(named_url_structure) == 1:
                 url_or_alias = named_url_structure[0]
                 if len(url_or_alias) == 0:
@@ -35,7 +36,8 @@ def get_targets(url_string):
                     if ':' in url_or_alias:
                         targets.update({ default_target_name: url_or_alias })
                     else:
-                        targets.update({ default_target_name: f'{alias_protocol}{url_or_alias}' })
+                        alias = url_or_alias.lower()
+                        targets.update({ default_target_name: f'{alias_protocol}{alias}' })
                 else:
                     print(f'warning: Default target is already defined: "{named_url}"', file=sys.stderr)
             else:
@@ -48,82 +50,65 @@ def send():
     sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8', line_buffering=True)
     sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8', line_buffering=True)
 
-    url_or_alias = os.environ.get('TEAMS_WEBHOOK')
-    targets = get_targets(url_or_alias)
-    if default_target_name in targets:
-        default_url = targets[default_target_name]
-    else:
-        default_url = None
-
     import argparse
     parser = argparse.ArgumentParser(description='Send message from /dev/stdin to Microsoft Teams\' incoming webhook')
-    parser.add_argument('-t', '--text', nargs=1, help=f'message instead of /dev/stdin')
-    parser.add_argument('-u', '--url', nargs=1, help=f'webhook url (default: {default_url})')
-    parser.add_argument('-n', '--name', nargs=1, help=f'webhook target name')
+    parser.add_argument('-n', '--name', nargs=1, default=None, help=f'webhook target name')
+    parser.add_argument('-u', '--url', nargs=1, default=None, help=f'webhook url')
     parser.add_argument('--list', action='store_true', help='list target names')
     parser.add_argument('--dry', action='store_true', help='dry run')
 
     args = parser.parse_args()
-
-    if args.url:
-        url_or_alias = args.url[0]
-        targets = get_targets(url_or_alias)
-
-    if not url_or_alias:
-        print('No webhook url ($TEAMS_WEBHOOK or -u parameter is necessary)', file=sys.stderr)
-        return 1
-
-    if args.list:
-        for name in targets.keys():
-            print(f'{name}: {targets[name]}')
-        return 1
 
     if args.name:
         name = args.name[0].lower()
     else:
         name = default_target_name
 
-    if name in targets.keys():
-        url_or_alias = targets[name]
-        while url_or_alias.startswith(alias_protocol):
-            if not url_or_alias[len(alias_protocol):] in targets.keys():
-                print(f'No such a webhook target name: {url_or_alias}', file=sys.stderr)
-                return 1
-            url_or_alias = targets[url_or_alias[len(alias_protocol):]]
-        url = url_or_alias
-    else:
-        if name == default_target_name:
-            print('Default webhook target name is not defined', file=sys.stderr)
-        else:
-            print(f'No such a webhook target name: {name}', file=sys.stderr)
+    url_or_alias = os.environ.get('TEAMS_WEBHOOK')
+    targets = get_targets(url_or_alias)
+
+    if args.list:
+        for name in targets.keys():
+            print(f'{name}: {targets[name]}')
         return 1
 
-    if args.text:
-        text = args.text[0]
+    if args.url:
+        url = args.url[0]
     else:
-        text = sys.stdin.read()
+        if name in targets.keys():
+            url_or_alias = targets[name]
+            while url_or_alias.startswith(alias_protocol):
+                if not url_or_alias[len(alias_protocol):] in targets.keys():
+                    print(f'No such a webhook target name: {url_or_alias}', file=sys.stderr)
+                    return 1
+                url_or_alias = targets[url_or_alias[len(alias_protocol):]]
+            url = url_or_alias
+        else:
+            if name == default_target_name:
+                print('Default webhook target name is not defined', file=sys.stderr)
+            else:
+                print(f'No such a webhook target name: {name}', file=sys.stderr)
+            return 1
 
-    first_line_matched = re.match(r'^(#[^\n\r]*)(\n|\r)', text)
-    if first_line_matched:
-        title = first_line_matched.group(1)
-        text = text[first_line_matched.end():]
-    else:
-        title = ''
-
+    text = sys.stdin.read()
     text = re.sub(r'[\n\r]', r'  \n', text)
 
     message = {
-        'type': 'message',
+        'text': text,
         'attachments': [
             {
-                'contentType': 'application/vnd.microsoft.teams.card.o365connector',
+                'contentType': 'application/vnd.microsoft.card.adaptive',
+                'contentUrl': None,
                 'content': {
-                    '@type': 'MessageCard',
-                    '@context': 'https://schema.org/extensions',
-                    'title': title,
-                    'sections': [
+                    '$schema': 'http://adaptivecards.io/schemas/adaptive-card.json',
+                    'type': 'AdaptiveCard',
+                    'version': '1.4',
+                    'body': [
                         {
-                            'text': text
+                            'type': 'TextBlock',
+                            'text': text,
+                            'wrap': True,
+                            'markdown': True
                         }
                     ]
                 }
@@ -133,13 +118,17 @@ def send():
 
     if args.dry:
         import json
-        print(f'url={url_or_alias}')
+        print(f'url={url}')
         print(json.dumps(message, indent=2, ensure_ascii=False))
         return 0
 
     res = None
     try:
-        res = requests.post(url_or_alias, json=message)
+        headers = {
+            'Content-Type': 'application/json'
+        }
+        data = message
+        res = requests.post(url, json=data, headers=headers)
     except Exception as e:
         print(e, file=sys.stderr)
         return 1
